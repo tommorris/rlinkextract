2ddea8e33365a3054c416ed820b1fff4e7880d60
Tom Morris <tom@tommorris.org>
2012-10-23 03:23:09 +0100

    fixing URL parsing that already has scheme.
    fixes #1

--- lib/rlinkextract.rb
+++ lib/rlinkextract.rb
@@ -1,4 +1,6 @@
 require "nokogiri"
+require "uri"
+
 class RLinkExtract
   def self.extract(text)
     re = Regexp.new("(?:\\@[_a-zA-Z0-9]{1,17})|(?:(?:(?:(?:http|https|irc)?:\\/\\/(?:(?:[!$&-.0-9;=?A-Z_a-z]|(?:\\%[a-fA-F0-9]{2}))+(?:\\:(?:[!$&-.0-9;=?A-Z_a-z]|(?:\\%[a-fA-F0-9]{2}))+)?\\@)?)?(?:(?:(?:[a-zA-Z0-9][-a-zA-Z0-9]*\\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|j[emop]|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|museum|m[acdeghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\\:\\d{1,5})?)(?:\\/(?:(?:[!#&-;=?-Z_a-z~])|(?:\\%[a-fA-F0-9]{2}))*)?)(?=\\b|\\s|$)")
@@ -12,11 +14,20 @@ class RLinkExtract
     doc.root.xpath('//text()[name(..) != "a"]').each do |textnode|
       t = textnode.to_s
       t.scan(re) do |replace|
-        t = t.sub(replace, "<a href=\"http://" + replace + "\">" + replace + "</a>")
+        t = t.sub(replace, "<a href=\"" + RLinkExtract.full_url(replace) + "\">" + replace + "</a>")
       end
       textnode.replace t
     end
 
     return doc
+  end
+
+  private
+  def self.full_url(url)
+    if URI.parse(url).scheme.nil?
+      return "http://" + url
+    else
+      return url
+    end
   end
 end
--- test/rlinkextract_test.rb
+++ test/rlinkextract_test.rb
@@ -4,5 +4,6 @@ require "./lib/rlinkextract.rb"
 class RLinkExtractTest < Test::Unit::TestCase
   def test_linkextract
     assert_equal 1, RLinkExtract.extract("<div>this is my place. tommorris.org</div>").search("a").size
+    assert_equal "http://tommorris.org", RLinkExtract.extract("<div>this is my place. http://tommorris.org</div>").search("a").first["href"]
   end
 end
